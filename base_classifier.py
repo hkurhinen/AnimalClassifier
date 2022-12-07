99e4f8808a39219343c1b9596ac2be321b7c9076
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2022-12-07 14:57:14 +0200

    Added feature for passing url instead of path for images and passing latitude and longitude along with the messages

--- base_classifier.py
+++ base_classifier.py
@@ -7,14 +7,18 @@ from pyspark.sql.functions import col
 from keras.applications import ResNet50
 from keras.utils import load_img, img_to_array
 from keras.applications.resnet import preprocess_input, decode_predictions
+from urllib.request import urlopen
+from io import BytesIO
+from datetime import datetime, timezone
+
 import numpy as np
 import requests
 
 from mqtt import MQTTUtils
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print >> sys.stderr, "Usage: base_classifier.py <broker url> <topic>"
+    if len(sys.argv) != 4:
+        print >> sys.stderr, "Usage: base_classifier.py <broker url> <topic> <server url>"
         exit(-1)
 
     sc = SparkContext(appName="AnimalClassifier")
@@ -24,21 +28,35 @@ if __name__ == "__main__":
 
     broker_url = sys.argv[1]
     topic = sys.argv[2]
+    server_url = sys.argv[3] 
 
     stream = MQTTUtils.createStream(ssc, broker_url, topic, username=None, password=None)
+
+    def loadImgFromUrl(url):
+      with urlopen(url) as image:
+        img = load_img(BytesIO(image.read()), target_size=(224, 224))
+
+      return img_to_array(img)
+
 
     def readStream(rdd):
       if not rdd.isEmpty():
         df = spark.read.json(rdd)
         print('Started the Process')
         print('Selection of Columns')
-        df = df.select('image').where(col("image").isNotNull())
-        path = df.collect()[0][0]
-        img = load_img(path, target_size=(224, 224))
-        x = img_to_array(img)
+        df = df.select(
+          'image',
+          'latitude',
+          'longitude').where(
+            col("image").isNotNull()
+          )
+        data = df.collect()
+        url = data[0][0]
+        latitude = data[0][1]
+        longitude = data[0][2]
+        x = loadImgFromUrl(url)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
-
         preds = model.predict(x)
         cs = decode_predictions(preds, top=3)[0]
         classifications = []
@@ -55,18 +73,19 @@ if __name__ == "__main__":
         #print(description)
         #print("Probability")
         #print(probablity)
+        utc_dt = datetime.now(timezone.utc)
+        iso_date = utc_dt.astimezone().isoformat()
 
         obj = { 
-          "id": 1,
-          "image": path,
-          "latitude": 61.667308,
-          "longitude": 27.359664,
-          "created": "2000-01-23T04:56:07.000+00:00",
-          "modified": "2000-01-23T04:56:07.000+00:00",
+          "image": url,
+          "latitude": latitude,
+          "longitude": longitude,
+          "created": iso_date,
+          "modified": iso_date,
           "classifications": classifications
         }
 
-        x = requests.post("http://localhost:8080/events", json = obj)
+        x = requests.post(server_url + "/events", json = obj)
         print(x.text)
 
     stream.foreachRDD( lambda rdd: readStream(rdd) )
