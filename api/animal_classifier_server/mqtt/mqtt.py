cb43cbad260d8606813bcf2a3e5541b6a17a8f9f
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2022-12-27 14:18:01 +0200

    Fixed bug with mqtt client keeping the connection open

--- api/animal_classifier_server/mqtt/mqtt.py
+++ api/animal_classifier_server/mqtt/mqtt.py
@@ -5,7 +5,7 @@ from paho import mqtt
 broker = 'localhost'
 port = 1883
 client_id = f'animal-classifier-server-{random.randint(0, 1000)}'
-client = None
+mqtt_client = None
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -15,13 +15,14 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
     # Set Connecting Client ID
     client = paho.Client(client_id)
-    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    #client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     #client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
-    client.is_connected
+    return client
 
 def get_mqtt_client():
-  if client is None or not client.is_connected:
-    connect_mqtt()
-  return client
+  global mqtt_client
+  if mqtt_client is None or not mqtt_client.is_connected:
+    mqtt_client = connect_mqtt()
+  return mqtt_client
